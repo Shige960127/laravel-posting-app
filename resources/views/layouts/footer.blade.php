97e2f0d48ab9ad195465e59046fef4e673b8f862
重森建人 <shige960127@shigemokerusMBP.local>
2023-03-13 22:41:28 +0900

    common of view parts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px;">
+<p class="text-muted small mb-0">&copy; 登校アプリ All rights reserved.</p>
+</footer>
